ecbf43f6aa0aa4154c69367f62362abacdeb6447
Joe S. <joe@skrd.io>
2026-01-02 11:42:25 +0000

    chore: set tsconfigRootDir in ESLint config
    Added tsconfigRootDir to ESLint parserOptions using the current directory to ensure correct TypeScript configuration resolution.

--- apps/book/eslint.config.ts
+++ apps/book/eslint.config.ts
@@ -2,10 +2,21 @@ import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 import prettier from "eslint-config-prettier/flat";
 import { defineConfig, globalIgnores } from "eslint/config";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    languageOptions: {
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+      },
+    },
+  },
   prettier,
   globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
 ]);
